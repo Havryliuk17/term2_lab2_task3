f24cf6edaff4722e1dc230f4fb88f775e05ed3dc
Havryliuk17 <116552018+Havryliuk17@users.noreply.github.com>
2023-02-28 22:14:39 +0200

    File with main script

---
+++ web_app/web.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect
+from web_back import get_coords
+from web_back import search_for_artist_name
+from web_back import get_top_1_song_of_artist
+import folium
+
+app = Flask(__name__)
+
+
+@app.route('/search4', methods=['POST'])
+
+def do_search():
+    phrase = request.form['phrase']
+    if search_for_artist_name(phrase) == None:
+        return render_template('mistake.html')
+    elif get_top_1_song_of_artist(phrase) == None:
+        return render_template('mistake.html')
+    coords = get_coords(phrase)
+
+    map = folium.Map(tiles= 'openstreetmap',
+                        location=[54.43, 26.09],
+                        zoom_start=3)
+    folium.LayerControl().add_to(map)
+
+    html = """Country name: {}<br>"""
+
+    
+    for i in (coords):
+            iframe = folium.IFrame(html=html.format(i[0]),
+                          width=200,
+                          height=100)
+            map.add_child(folium.Marker(location=[i[1][0], i[1][1]],
+                                    popup=folium.Popup(iframe),
+                                    icon=folium.Icon(color = "red")))
+
+    return render_template('results.html', map=map._repr_html_())
+
+@app.route('/')
+@app.route('/entry')
+def entry_page():
+    return render_template('entry.html', the_title='Welcome to the available markets looker')
+
+if __name__ == '__main__':
+    app.run(debug = True)
